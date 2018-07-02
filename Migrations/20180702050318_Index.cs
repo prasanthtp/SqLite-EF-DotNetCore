ea29d35f5b7d95c71cdefe85d8bb8f972afc5719
CORIN\ppillai <Prasanth.Pillai@coringroup.com>
2018-07-02 15:04:23 +1000

    Add project files.

---
+++ Migrations/20180702050318_Index.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp.SQLite.Migrations
+{
+    public partial class Index : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_Url",
+                table: "Blogs",
+                column: "Url");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_Url",
+                table: "Blogs");
+        }
+    }
+}
